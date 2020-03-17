dd868a3a5b56ec0ff1f553744a8526418f9ef1d2
Ninhow <csn7083@csnnet.int>
2020-03-17 14:52:15 +0100

    Project as API version!

---
+++ src/main/java/company/services/Service.java
@@ -0,0 +1,9 @@
+package company.services;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("AngularElement")
+public class Service extends Application {
+
+}
