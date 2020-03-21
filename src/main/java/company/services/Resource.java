b7161091a7f4e24aef5d4cda0094a2874ebee507
Ninhow <allamoolsson09@gmail.com>
2020-03-21 12:03:49 +0100

    Added a angular project and tested

--- src/main/java/company/services/Resource.java
+++ src/main/java/company/services/Resource.java
@@ -5,6 +5,8 @@ import java.util.Properties;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
 @Path("properties")
@@ -13,7 +15,23 @@ public class Resource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Properties getProperties() {
+        System.out.println("HEJHEJ");
         return System.getProperties();
+    }
+
+    // @POST
+    // @Path("/{id}")
+    // @Consumes(MediaType.APPLICATION_JSON)
+    // public Properties getProperties(@PathParam("id") String id) {
+    //     System.out.println("HEJHEJ");
+    //     return System.getProperties();
+    // }
+
+    @POST
+    @Path("/update")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void updateHook(){
+        System.out.println("Update hook called!");
     }
 
 }
